4f8b6a42b23a3213a2266e5eeb5beec90415d5c8
gold_rush <bartekchyra@gmail.com>
2025-12-01 17:05:18 +0100

    Initial commit

---
+++ src/main/java/edu/io/net/Main.java
@@ -0,0 +1,33 @@
+package edu.io.net;
+
+import edu.io.net.GameServerConnector;
+import edu.io.net.SocketConnector;
+import edu.io.net.command.Handshake;
+import edu.io.net.command.JoinGame;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        var gsc = new GameServerConnector("tcp://localhost:1313", new SocketConnector());
+        gsc.connect();
+        if (!gsc.isConnected()) {
+            System.err.println("Could not connect to server");
+            return;
+        }
+
+        gsc.issueCommand(new Handshake.Cmd("1.1.17"), res -> {
+            System.out.println(res);
+        });
+
+        var in = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        var name = in.nextLine();
+
+        gsc.issueCommand(new JoinGame.Cmd(name), res -> {
+            System.out.println(res);
+        });
+
+        new Scanner(System.in).nextLine();
+    }
+}
